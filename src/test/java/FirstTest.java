9d4ae590bec613074dcd289e32321ca7433cae2d
C Vera <cvera08@gmail.com>
2020-09-24 20:36:56 -0300

    Run Basic Selenium test with testNG.xml or ReadyToGo.java

---
+++ src/test/java/FirstTest.java
@@ -0,0 +1,30 @@
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class FirstTest extends BaseTest
+{
+    @Test
+    public void FacebookTest() throws Exception {
+        Reporter.log(String.format("Facebook Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("http://www.facebook.com");
+        Assert.assertEquals(getDriver().getTitle(), "Facebook - Log In or Sign Up");
+        Reporter.log(String.format("Facebook Test Ended %s",Thread.currentThread().getId()),true);
+    }
+
+    @Test
+    public void GmailTest() throws Exception {
+        Reporter.log(String.format("Gmail Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("http://www.gmail.com");
+        Assert.assertEquals(getDriver().getTitle(), "Gmail");
+        Reporter.log(String.format("Gmail Test Ended %s",Thread.currentThread().getId()),true);
+    }
+
+    @Test
+    public void InstagramTest() throws Exception {
+        Reporter.log(String.format("Instagram Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("https://www.instagram.com/");
+        Assert.assertEquals(getDriver().getTitle(), "Instagram");
+        Reporter.log(String.format("Instagram Test Ended %s",Thread.currentThread().getId()),true);
+    }
+}
---
+++ src/test/java/SecondTest.java
@@ -0,0 +1,22 @@
+import org.testng.Assert;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SecondTest extends BaseTest{
+
+    @Test
+    public void MyntraTest() throws Exception {
+        Reporter.log(String.format("Myntra Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("https://www.myntra.com/");
+        Assert.assertEquals(getDriver().getTitle(), "Online Shopping for Women, Men, Kids Fashion & Lifestyle - Myntra");
+        Reporter.log(String.format("Myntra Test Ended %s",Thread.currentThread().getId()),true);
+    }
+
+    @Test
+    public void ThoughtWorksTest() throws Exception {
+        Reporter.log(String.format("ThoughtWorks Test Started %s",Thread.currentThread().getId()),true);
+        getDriver().navigate().to("https://www.thoughtworks.com/");
+        Assert.assertEquals(getDriver().getTitle(), "ThoughtWorks: A Global Software Consultancy | ThoughtWorks");
+        Reporter.log(String.format("ThoughtWorks Test Ended %s",Thread.currentThread().getId()),true);
+    }
+}
