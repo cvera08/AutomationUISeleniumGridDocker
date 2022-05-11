9dc7934d40901dcb70da2fda51a76f01924b4909
cvera08 <cvera08@gmail.com>
2022-05-10 22:45:32 -0300

    Updates since previous mvn version is not supported now and updates in the assert of page titles

--- src/test/java/ReadyToGo.java
+++ src/test/java/ReadyToGo.java
@@ -2,6 +2,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.BrowserType;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 
 import java.net.MalformedURLException;
@@ -9,17 +10,22 @@ import java.net.URL;
 
 public class ReadyToGo {
 
+    WebDriver driver;
+
     @Test
     public void testApp() throws MalformedURLException, InterruptedException {
+
         DesiredCapabilities cap = new DesiredCapabilities();
-//        cap.setBrowserName(BrowserType.CHROME);
-        cap.setBrowserName(BrowserType.FIREFOX);
-        WebDriver driver = new RemoteWebDriver(new URL("http://localhost:4446/wd/hub"), cap);
+        cap.setBrowserName(BrowserType.CHROME);
+        //cap.setBrowserName(BrowserType.FIREFOX);
+        driver = new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), cap);
 
         driver.get("http://www.google.com");
+        Thread.sleep(1000);
+    }
 
-        Thread.sleep(5000);
-
+    @AfterTest
+    public void afterTest() {
         driver.quit();
     }
 
